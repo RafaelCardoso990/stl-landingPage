79551837e7cb323935db37abea04ff33b9ac5ada
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2023-12-12 15:01:06 -0300

    att

--- src/components/buttonWpp/ButtonCta.js
+++ src/components/buttonWpp/ButtonCta.js
@@ -6,7 +6,7 @@ const { Main } = style;
 export default function ButtonCta(props) {
   function redirectToWhatsApp() {
     const phoneNumber = "+5531975594540";
-    const message = "Olá, estou entrando em contato para saber mais sobre a escursão para o STL Festival.";
+    const message = "Olá, estou entrando em contato para saber mais sobre a excursão para o STL Festival.";
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.location.href = url;
   }
